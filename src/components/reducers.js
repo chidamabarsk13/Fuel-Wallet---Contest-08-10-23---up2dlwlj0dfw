2bc722e0af0160f7ede5b0ef05f3699e8b654f92
chidamabarsk13 <142134688+chidamabarsk13@users.noreply.github.com>
2023-10-08 10:32:37 +0530

    Create reducers.js

---
+++ src/components/reducers.js
@@ -0,0 +1,20 @@
+const reducer=(state, action) => {
+  switch(action.type){
+      case"add";
+      const newUser={
+        id: uuidv4(),
+        name: action.name,
+      };
+      return[...state, newUser];
+
+    case "fuel";
+      if(action.balance >= 50){
+        action.setBalance(action.balance - 50);
+      }
+      return state;
+
+  default;
+      return state;
+  }
+};
+export default reducer;
